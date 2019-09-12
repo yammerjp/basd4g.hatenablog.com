8f449e99504d6a0a2bfa533cd11bfb90a1a07793
Keisuke Nakayama <baskk4.dt@gmail.com>
2019-09-13 00:13:06 +0900

    checkout -b

--- new.sh
+++ new.sh
@@ -29,7 +29,34 @@ fi
 
 # 引数(記事id)のブランチ名が無いことを確認
 git fetch
-git branch -a 
+git branch -a | grep -e draft/$1$
+if [ $? = 0 ]; then
+	echo "grepにマッチした.. 既に存在するブランチ名(引数,記事id)やで。"
+  exit 1
+fi
 
+
+# ブランチを切る
+git stash
 git checkout master
-git checkout -b 
+git checkout -b draft/$1
+
+# index.mdを作る
+mkdir $1
+touch $1/index.md
+
+# index.mdに初期値を入れる
+echo '---' >> $1/index.md
+echo title: $1 >> $1/index.md
+echo date: `date '+%Y-%m-%d %T'` >> $1/index.md
+echo tags: >> $1/index.md
+echo '---' >> $1/index.md
+echo '' >> $1/index.md
+echo "## $1" >> $1/index.md
+echo "" $1/index.md
+
+git add $1/index.md
+git commit -m "記事$1を追加 (new.sh)"
+git push --set-upstream origin draft/$1
+echo "FINISH! Add git branch draft/$1, Git commit&push, Add file $1/index.md"
+code $1/index.md
