e575f5c9baa237a641b8227423e62d920529dd11
Keisuke Nakayama <baskk4.dt@gmail.com>
2019-09-23 02:13:25 +0900

    わかりやすさを重視したdev.sh内の修正 README.mdなどが/assets/article/内にコピーされるようになったが、記事ディレクトリでないと生成される静的サイトには影響ないので問題ない。

--- dev.sh
+++ dev.sh
@@ -6,7 +6,7 @@ git clone https://github.com/basd4g/green-turtle.git
 rm -rf green-turtle/assets
 mkdir -p green-turtle/assets/article
 rsync -b ./ green-turtle/assets/article --exclude 'green-turtle'
-ls ./ | grep -v green-turtle | grep -v dev.sh | grep -v new.sh | grep -v README.md | xargs -IXXXX cp -r XXXX green-turtle/assets/article/
+ls ./ | grep -v green-turtle | xargs -IXXXX cp -r XXXX green-turtle/assets/article/
 cd green-turtle
 yarn install
 yarn dev
